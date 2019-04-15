4028710954fa0049cf6a288e6da063e59ef056a5
Сергей <zergend@gmail.com>
2019-04-15 16:58:15 +0300

    1

--- ReadWords/Form1.cs
+++ ReadWords/Form1.cs
@@ -11,8 +11,7 @@ using System.Windows.Forms;
 
 
 using YandexDiskNET;
-
-
+using System.Text.RegularExpressions;
 
 namespace ReadWords
 {
@@ -95,22 +94,29 @@ namespace ReadWords
             {
                 // Open document 
                 string originalfilename = System.IO.Path.GetFullPath(openFile1.FileName);
-                
+
+                /* countTitle: № поиска подстроки = № элемента в заголовке
+                 * 1 - тип документа (начальное значение)
+                 * 2 - дата документа   \
+                 *   +                   > как правило, в одном абзаце!!!
+                 *   - номер документа  /
+                 * 3 - название документа
+                 */
+                int countTitle = 1; 
+
                 if (openFile1.CheckFileExists && new[] { ".docx", ".doc", ".txt", ".rtf" }.Contains(Path.GetExtension(originalfilename).ToLower()))
                 {
-                   // Microsoft.Office.Interop.Word.Application wordObject = new Microsoft.Office.Interop.Word.Application();
                     object File = originalfilename;
                     object nullobject = System.Reflection.Missing.Value;
                     Microsoft.Office.Interop.Word.Application wordobject = new Microsoft.Office.Interop.Word.Application();
                     wordobject.DisplayAlerts = Microsoft.Office.Interop.Word.WdAlertLevel.wdAlertsNone;
                     Microsoft.Office.Interop.Word._Document docs = wordobject.Documents.Open(ref File, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject);
-                    //docs.ActiveWindow.Selection.WholeStory();
-                    
-                    //docs.ActiveWindow.Selection.Copy();
+
 
                     Microsoft.Office.Interop.Word.Range wordContentRange = docs.Content;
 
                     string textFromWordDocument = string.Empty;
+                    string strDocTitle = string.Empty;
 
                     for (int i = 1; i <= wordContentRange.Paragraphs.Count; i++ )
                     {
@@ -120,24 +126,86 @@ namespace ReadWords
                             textBox.Text += i.ToString() + ": !___ПУСТОЙ ПАРАГРАФ___!" + "\r\n";
                         } else
                         {
-                            if ()
+                            // textBox.Text += i.ToString() + ": " + textFromWordDocument + "\r\n";
+                            string title = docTitle(textFromWordDocument, countTitle);
+
+                            switch (countTitle)
+                            {
+                                case 1:
+                                case 2:
+                                case 3: // тип документа + номер + дата + название
+                                    if (title != string.Empty)
+                                    {
+                                        strDocTitle += title;
+                                        countTitle++;
+                                    }
+                                    break;
+                                default:
+                                    // 
+                                    break;
+                            }
+
+                            /* if ()
                                 tableLayoutPanel1.SetRow()
                             textBox.Text += i.ToString() + ": " + textFromWordDocument + "\r\n";
-                            
+                            */
+
                         }
                         
                     }
-                    //richTextBox1.Text = textFromWordDocument;                   
 
-                    //richTextBox1.Paste();
+                    textBox.Text += strDocTitle + "\r\n";
 
                     docs.Close(ref nullobject, ref nullobject, ref nullobject);
                     wordobject.Quit(ref nullobject, ref nullobject, ref nullobject);
 
-
                     MessageBox.Show("file loaded");
                 }
             }
+        }
+
+        static string docTitle(string s, int i)
+        {
+            string sT = string.Empty;
+            switch (i)
+            {
+                case 1:
+                    Regex regex1 = new Regex(@"решен(\w*)|постановл(\w*)|распоряж(\w*)");
+                    MatchCollection matches1 = regex1.Matches(s.ToLower());
+                    if (matches1.Count > 0)
+                    {
+                        foreach (Match match in matches1)
+                            sT += " " + match.Value;
+                        sT = sT.Trim();
+                        return sT.ToUpper();                        
+                    }
+                    else
+                    {
+                        return string.Empty;
+                    }
+                case 2:
+                    Regex regex3 = new Regex(@"(\d+)\D*(январ[ьея]|феврал[ьея]|март[еа]?|апрел[ьея]|ма[йея]|ию[нл][яье]|август[еа]?|(?:сент|окт|но|дек)[ая]бр[яье])(\D*\d+)\D*(\d+)", RegexOptions.IgnoreCase);
+                    
+                    MatchCollection matches3 = regex3.Matches(s);
+                    if (matches3.Count > 0)
+                    {
+                        foreach (Match match in matches3)
+                        {
+                            GroupCollection groups = match.Groups;
+                            sT += " № " + groups[4].Value.Trim();
+                            sT += " от " + groups[1].Value.Trim();
+                            sT += " " + groups[2].Value.Trim();
+                            sT += " " + groups[3].Value.Trim();
+                        }
+
+                        sT = sT.ToLower();
+                    }
+                    return sT;
+                case 3:
+                    return " " + s.Trim();
+                default:
+                    return string.Empty;
+            }            
         }
     }
 }
